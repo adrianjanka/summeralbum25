4c51cdb2d109fa526bbe5ce27c40e805cc82f32e
adrianjanka <adi.janka@bluewin.ch>
2025-09-18 13:35:26 +0200

    changes for live

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script>
   import '$lib/styles/xp.css';
+  import { base } from '$app/paths';
 </script>
 
-<div class="bliss"></div>
+<div class="bliss" style={`background:url('${base}/bg/bliss.webp') center/cover no-repeat`}></div>
 <main style="position:fixed; inset:0;">
   <slot />
 </main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
+const dev = process.env.NODE_ENV === 'development';
 const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/summeralbum' : ''
-    },
-    prerender: { entries: ['*'] }
+      base: dev ? '' : '/summeralbum'
+    }
   }
 };
 
